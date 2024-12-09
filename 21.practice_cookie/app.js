aa0890c7c781165c8c151423e202eb2bc92ffd1c
yundda <vvvzzang1218@naver.com>
2024-12-09 17:53:59 +0900

    cookie practice

---
+++ 21.practice_cookie/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+const PORT = 8080;
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// TODO : 쿠키 미들웨어 설정
+app.use(cookieParser("secret"));
+const cookieConfig = {
+  maxAge: 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  signed: true,
+};
+
+app.get("/", (req, res) => {
+  // TODO : 쿠키값 가져오기 및 index.ejs에 보내기
+  res.render("index", { popup: req.signedCookies.mycookie });
+  console.log(req.signedCookies);
+});
+
+app.post("/set-cookie", (req, res) => {
+  // TODO : 쿠키 생성하기
+  res.cookie("mycookie", "cookie", cookieConfig);
+  console.log(req.signedCookies);
+  // { mycookie: 'cookie' }
+  res.send("쿠키 생성 성공");
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
